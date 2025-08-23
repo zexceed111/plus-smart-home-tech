80e124c3ef8d9c88d29b92dbbbcfa353cd276330
Kazurov Kostya <kazurovzxc@mail.ru>
2025-08-23 23:18:47 +0200

    SensorEnum

---
+++ telemetry/collector/src/main/java/ru/yandex/practicum/model/sensor/enums/SensorEventType.java
@@ -0,0 +1,9 @@
+package ru.yandex.practicum.model.sensor.enums;
+
+public enum SensorEventType {
+    CLIMATE_SENSOR_EVENT,
+    LIGHT_SENSOR_EVENT,
+    MOTION_SENSOR_EVENT,
+    SWITCH_SENSOR_EVENT,
+    TEMPERATURE_SENSOR_EVENT
+}
