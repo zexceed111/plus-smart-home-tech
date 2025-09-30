e35dc278c83e50608bef2d08818bcc7b7681b14b
Kazurov Kostya <kazurovzxc@mail.ru>
2025-09-30 11:23:57 +0200

    .

--- commerce/interaction-api/src/main/java/ru/yandex/practicum/dto/ShoppingCartDto.java
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/dto/ShoppingCartDto.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.dto;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
@@ -14,5 +15,6 @@ public class ShoppingCartDto {
     UUID id;
     
     @NotEmpty(message = "Products map cannot be empty")
-    Map<UUID, Long> products;
+    @Builder.Default
+    Map<UUID, Long> products = new HashMap<>();
 }
